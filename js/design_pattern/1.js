61ae9b506a28a8196d29b7ea2642e30fadf00096
wu-hu-1201 <1871516072@qq.com>
2020-07-16 18:48:47 +0800

    对象字面量

---
+++ js/design_pattern/1.js
@@ -0,0 +1,45 @@
+//       D
+//20000   C  16
+//        B  18
+//        D  20
+/*
+*功能：计算年收益
+*@author whb
+*@data 2020-7-16
+*@params {salary  工资  number}
+*{level  评级  string}
+*@return   年收益 number 
+*/
+
+
+
+
+
+
+function calculateBonus(salary, level = 'C') {
+    console.log(level);
+  
+    if (typeof +salary != 'number' || salary < 0) {
+      throw new TypeError('工资必须是整数');
+    }
+
+    if(['A', 'B', 'C', 'D', 'S'].indexOf(level) == -1) {
+        throw new Error("错误的等级")
+    }
+    
+    salary = parseInt(salary);
+  
+    if (level == 'D') {
+      return salary * 12;
+    } else if (level == 'B') {
+      return salary * 18
+    } else if (level == 'A') {
+      return salary * 20
+    }else if (level == 'S'){
+        return salary * 100
+    }
+  
+    return salary *  16
+  }
+  
+  console.log(calculateBonus('20000', 'D'));
