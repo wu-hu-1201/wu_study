b97de658867794ef3d93e320812fd1c4db104036
wu-hu-1201 <1871516072@qq.com>
2020-07-21 22:50:47 +0800

    微信小程序：零食销售

---
+++ wxapp1/pages/index/index.js
@@ -0,0 +1,70 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    markers: [{
+      iconPath: "/resources/bike.png",
+      id: 0,
+      latitude: 28.71590,
+      longitude: 115.828026,
+      width: 25,
+      height: 25
+    }],
+    markers: [{
+      iconPath: "/resources/bike.png",
+      id: 1,
+      latitude: 28.71580,
+      longitude: 115.828066,
+      width: 25,
+      height: 25
+    }],
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
