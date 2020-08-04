e7f06c9255228c588555492a9b18e4a61d453904
wu-hu-1201 <1871516072@qq.com>
2020-08-04 11:40:27 +0800

    规避冲突，自执行函数

---
+++ js/SCOPE/fn-scope/3.js
@@ -0,0 +1,22 @@
+
+// var a = 2 
+// function foo() {
+//     var a = 3
+//     console.log(a)
+// }
+// foo()
+// console.log(a)
+
+
+
+//自执行函数
+
+var a = 2;
+(function foo(){  //函数表达式
+    var a = 3
+    console.log(a)
+})()
+
+console.log(a)
+
+//自执行函数是一个很好的隐藏作用域的方式
