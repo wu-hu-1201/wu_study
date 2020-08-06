4ad6af633138bb0b2fc360509fba0a26d0ab6bd5
wu-hu-1201 <1871516072@qq.com>
2020-08-06 18:04:32 +0800

    变量提升

---
+++ es6/Variable promotion/let_const(变量提升)/let_const(变量提升).js
@@ -0,0 +1,59 @@
+//var name
+//var obj = {}
+//console.log("name")
+//ar name = "张三"
+//name = "张三"
+
+
+//console.log(name)
+//let name = "吴"
+
+
+// for(let i = 0;i < 10;i++) {
+//     //console.log(i)
+// }
+// console.log(i)
+
+
+// var a = []
+// var i
+// for(i = 0;i <10 ;i++){
+//     a[i] = function() {
+//         console.log(i)
+//     }
+// }
+// a[6]()
+
+
+// for(let i = 0; i < 3; i++) { //父作用域
+//     let i = 'abc'  //子作用域
+//     console.log(i)
+// }
+
+// 暂时性死区
+// var tmp = 123;
+
+// if(true) {
+//     tmp = 'abc'
+//     let tmp
+// }
+
+//   var a ='a'
+//   var a = 'aa'
+//   console.log(a)
+
+
+
+// const num= 1234
+// num = 1000
+// console.log(num)  //const声明的变量是不可更改的
+
+// if(true) {
+//     console.log(age)
+//     const age = 18
+// }
+
+
+const message = 'Hello World'
+const message = 'hello'
+console.log(message)
