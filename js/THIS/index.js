3baa51df7f11324ccbae5999b9a70bf7e91955d5
wu-hu-1201 <1871516072@qq.com>
2020-08-07 14:26:42 +0800

    this

---
+++ js/THIS/index.js
@@ -0,0 +1,88 @@
+// function identify(context) {
+//     return context.toUpperCase()
+// }
+
+
+// function speak(context) {
+//     let greeting = "Hello, I'm" + identify(context)
+//     console.log(greeting)    //--->Hello, I'mME
+// }
+
+// speak('me')
+
+
+
+// function identify() {
+//     return this.name.toUpperCase()
+// }
+
+
+// function speak() {
+//     let greeting = "Hello, I'm" + identify.call(this)
+//     console.log(greeting)    //--->Hello, I'mME
+// }
+//  let me = {
+//      name: 'wn'
+//  }
+
+//  speak.call(me)
+
+
+//  this 所处的词法作用域在哪里生效了， this作用域就指向哪里
+
+// function foo() {
+//     let person = {
+//         name: 'wn',
+//         age: '20'
+//     }
+//     console.log(this.person.age)
+// }
+// foo()
+
+
+// function foo() {
+//     console.log(this.a)   //-->2
+// }
+// var a = 2
+// foo()
+
+
+// function foo() {
+//     console.log(this.a)   
+// }
+// var a = 2
+// (function () {
+//     "use strict"
+//     foo()  //严格模式下的foo的调用，与位置无关
+// })()
+
+
+// function foo() {
+//     console.log(this.a)  //obj.a
+// }
+// var obj2 = {
+//     a: 3,
+//     foo: foo
+// }
+// var obj1 = {
+//     a: 2,
+//     obj2: obj2
+// }
+// obj1.obj2.foo()  //foo此刻被obj所拥有/包含
+
+
+function foo() {
+    console.log(this.a)
+}
+function doFoo(fn) {  //参数传递其实又称之为隐式赋值
+    fn()
+}
+var obj = {
+    a: 2,
+    foo: foo
+}
+
+// var bar = obj.foo;
+var a = 'hello'
+bar()
+doFoo(obj.foo)
