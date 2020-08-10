6e093937f4246f07281e92a3c9ecc59a4c68fab0
wu-hu-1201 <1871516072@qq.com>
2020-08-10 18:46:33 +0800

    图片上传

--- serverless/wx-db/miniprogram/pages/index/index.js
+++ serverless/wx-db/miniprogram/pages/index/index.js
@@ -4,6 +4,30 @@ const db = wx.cloud.database();
 const productsCollection = db.collection('products');
 
 Page({
+  uploadAvatar() {
+    //  上传图片  选择上传图片类型
+    //  图片
+    //  上传到云服务器
+    //  硬件
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],  //原图  压缩图
+      sourceType: ['album', 'camera'],  //选择相册还是相机拍照上传
+      success: res => {
+        console.log(res.tempFilePaths);
+        for (let filePath of res.tempFilePaths) {
+          let currentFileName = Math.floor(Math.random()*1000000).toString() + '.png';
+          wx.cloud.uploadFile({
+            cloudPath: currentFileName,
+            filePath,
+            success: res => {
+              console.log(res)
+            }
+          })
+        }
+      }
+    })
+  },
   data: {
     products: []
   },
