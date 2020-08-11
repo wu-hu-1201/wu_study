b2da55b971afdd3da78322b806bc20173482023a
wu-hu-1201 <1871516072@qq.com>
2020-08-11 18:48:15 +0800

    闭包

---
+++ js/closure/4.js
@@ -0,0 +1,39 @@
+function test() {
+    var food = 'apple'
+    function a() {
+        console.log(food)
+        food = 'banana'
+    }
+    function b() {
+        console.log(food)
+    }
+}
+
+// a()
+// b()
+
+// 想一个办法把这两个函数真的返回出来
+function fruit() {
+    var food = 'apple'  // food 一直被缓存
+    var obj = {
+        eatFood: function() {
+            if (food != '') {
+                console.log('I am eating' + food)
+                food = ''
+            } else {
+                console.log('There is nothing')
+            }
+        },
+        pushFood: function(myFood) {
+            food = myFood
+        }
+    }
+    return obj
+}
+var person = fruit()
+person.eatFood()
+person.eatFood()
+person.pushFood('banana')
+person.pushFood()
+
+// 为什么能连贯性的修改
