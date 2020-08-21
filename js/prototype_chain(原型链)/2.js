cb39aac4a0f35e2ea75452d14b977ae7a0c2fd54
wu-hu-1201 <1871516072@qq.com>
2020-08-21 14:51:16 +0800

    原型链

---
+++ js/prototype_chain(原型链)/2.js
@@ -0,0 +1,23 @@
+
+Person.prototype = {
+    // name: 'mingguo',
+    // sayName: function() {
+    //   console.log(this.name)
+    // }
+    height: 100
+  }
+  function Person() {
+    // this.name = 'yuanlong'
+    this.eat = function() {
+      this.height++
+    }
+  }
+  var person = new Person()
+  
+  // console.log(person.sayName()) // mingguo
+  
+  // console.log(person.sayName()) // yuanlong
+  
+  person.eat()
+  console.log(person.height) // 101
+  console.log(Person.prototype.height) // 100
