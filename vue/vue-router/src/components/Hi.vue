e187638599da9ed2a94597dd1813deef58810e27
wu-hu-1201 <1871516072@qq.com>
2020-08-26 18:37:41 +0800

    vue-router

--- vue/vue-router/src/components/Hi.vue
+++ vue/vue-router/src/components/Hi.vue
@@ -4,6 +4,11 @@
       <router-link :to="{name: 'Hi1', params:{name: 'ly'}}">hi一</router-link>
       <span style="display:inline-block; width:50px"></span>
       <router-link to="/hi/hi2">hi二</router-link>
+      <span style="display:inline-block; width:50px"></span>
+      <router-link to="/abc">404</router-link>
+      <div>
+          {{$route.params.name}}
+      </div>
       <router-view></router-view>
   </div>
 </template>
@@ -15,6 +20,14 @@ export default {
         return {
             msg: 'Hi, I am wn'
         }
+    },
+    beforeRouteEnter: (to, from, next) => {
+        console.log('准备进入目标')
+        next()
+    },
+    beforeRouteLeave: (to, from, next) => {
+        console.log('准备离开目标')
+        next()
     }
 }
 </script>
--- vue/vue-router/src/components/Hi2.vue
+++ vue/vue-router/src/components/Hi2.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="hi">
-      我是hi2界面
+      <p>我是hi2界面</p><br/>
+      <button @click="goBack">返回上一个界面</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  },
+  methods: {
+    goBack () {
+      // 编程式路由跳转
+      this.$router.push({ path: '/abc', query: {name: 'wn'} })
+      // this.$router.push({ path: 'abc', params: {name: 'wn'} })
+    }
+  }
 }
 </script>
 
