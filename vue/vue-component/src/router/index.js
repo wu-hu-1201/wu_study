f2aef08ef5d0658ec8e25d96f8771764b6ad1839
wu-hu-1201 <1871516072@qq.com>
2020-08-27 17:11:54 +0800

    vue-component

---
+++ vue/vue-component/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: () => import('@/views/Main.vue')
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: () => import('@/views/Detail.vue')
+  },
+  {
+    path: '/methods',
+    name: 'Methods',
+    component: () => import('@/views/Methods.vue')
+  },
+  {
+    path: '/watch',
+    name: 'Watch',
+    component: () => import('@/views/Watch.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
