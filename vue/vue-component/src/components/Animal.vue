c606bc1a33e9b2cba7e0f4fd7f668f34951e9ae2
wu-hu-1201 <1871516072@qq.com>
2020-08-27 17:12:38 +0800

    vue-component

---
+++ vue/vue-component/src/components/Animal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="animal">
+      <p>的宠物:{{babyName}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        babyName: {
+            type: String,
+            default: '小白'
+        }
+    },
+    data() {
+        return {
+            baby: '小白'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-component/src/components/User.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="user">
+     <p>{{userName}}</p>
+     <button @click="handdle">按钮</button>
+     <button @click="handdle1">养新的宠物</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['userName'],
+    data() {
+        return {
+            name: '蜗牛',
+            title: '我是子组件里的title',
+            babyName: '小黑'
+        }
+    },
+    methods: {
+        handdle() {
+            console.log('ok')
+            this.$emit('titleChange', this.title)
+        },
+        handdle1() {
+            console.log('可以的')
+            this.$emit('babyChange', this.babyName)
+        }
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
