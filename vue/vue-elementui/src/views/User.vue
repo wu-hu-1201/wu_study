72a6246ecdb89dac239c13fc4156a9216830f43a
wu-hu-1201 <1871516072@qq.com>
2020-08-31 21:18:01 +0800

    vue-elementui

---
+++ vue/vue-elementui/src/views/User.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="user">
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="基友姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="age"
+        label="年龄"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tableData: [
+                {
+                    name: '蜗牛',
+                    age: 22,
+                   address: '上海市普陀区金沙江路 1518 弄'
+                },
+                {
+                    name: '皮皮',
+                    age: 20,
+                    address: '上海市普陀区金沙江路 1518 弄'
+                },
+                {
+                    name: '元隆',
+                    age: 21,
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
