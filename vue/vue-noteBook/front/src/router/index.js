972e1f067202a4fa2d28928575a3cb48618627dd
wu-hu-1201 <1871516072@qq.com>
2020-09-16 09:50:16 +0800

    vue全栈实战项目

---
+++ vue/vue-noteBook/front/src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'StarNotes',
+    component: () => import('@/views/StarNotes.vue'),
+    meta: {
+      title: '星辰日记'
+    }
+  },
+  {
+    path: '/StarBanner',
+    name: 'StarBanner',
+    component: () => import('@/views/StarBanner.vue'),
+    meta: {
+      title: '欢迎'
+    }
+  },
+  {
+    path: '/StarLogin',
+    name: 'StarLogin',
+    component: () => import('@/views/StarLogin.vue'),
+    meta: {
+      title: '登录'
+    }
+  },
+  {
+    path: '/StarRegister',
+    name: 'StarRegister',
+    component: () => import('@/views/StarRegister.vue'),
+    meta: {
+      title: '注册'
+    }
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
