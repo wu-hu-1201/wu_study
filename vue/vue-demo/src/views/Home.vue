872bda5f99fe65de9dce1952ecb98d4267f4b58d
wu-hu-1201 <1871516072@qq.com>
2020-09-30 17:03:22 +0800

    vue-demo

---
+++ vue/vue-demo/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+      <p>首页</p>
+      <HomeChild />
+  </div>
+</template>
+
+<script>
+import HomeChild from '@/components/HomeChild.vue'
+export default {
+    components: {
+        HomeChild
+    }
+}
+</script>
+
+<style>
+
+</style>
