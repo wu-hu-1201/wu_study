07310207c2506cf947a9130614eb1a9c57ae21c8
wu-hu-1201 <1871516072@qq.com>
2020-10-09 19:25:29 +0800

    vue-music

---
+++ vue/vueMusic/vue-music/src/api/index.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const vue = new Vue()
+
+// axios 配置
+axios.defaults.timeout = 10000  // axios 请求时长
+axios.defaults.baseURL = 'http://localhost:3000'
+
+// 返回状态判断 （响应拦截）
+axios.interceptors.response.use(
+    (res) => {
+        if (res.data.code !== 200) {
+            vue.$toast('网络异常')
+            return Promise.reject(res)
+        }
+        return res.data
+    },
+    (error) => {
+        vue.$toast('服务器异常')
+        return Promise.reject(error)
+    }
+)
+
+export function fetchGet(url, param) {
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: param
+        })
+        .then(
+            response => {
+            resolve(response)
+            },
+            err => {
+                reject(err)
+            }
+        )
+        .catch(error => {
+            reject(error)
+        })
+    })
+}
+
+
+export default {
+    //  用户登陆
+    LOgin(params) {
+        return fetchGet('/login', params)
+    },
+    // 热门搜索
+    HotSearchKey() {
+        return fetchGet('/search/hot')
+    },
+    // music 搜索
+    MusicSearch(params) {
+        return fetchGet('/search', params)
+    },
+    // 获取歌曲url
+    MusicUrl(id) {
+        return fetchGet('/song/url', {
+            id
+        })
+    }
+}
