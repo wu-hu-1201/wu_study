daffe9a9c6d452617b5b197a9d54648ff806abe3
wu-hu-1201 <1871516072@qq.com>
2020-10-20 17:33:57 +0800

    myKoa

---
+++ NODE/myKoa/index.js
@@ -0,0 +1,29 @@
+// const http = require('http')
+
+// const request = require("./request");
+
+// const server = http.createServer((req, res) => {
+//     res.end('hello world')
+// })
+
+// server.listen(3000)
+
+
+// const Koa = require('koa')
+// const app = new Koa()
+
+// app.use((ctx, next) => {
+//     ctx.body = 'hello'
+// })
+
+// app.listen(3000)
+
+
+let Koa = require('./lib/application')
+let app = new Koa()
+
+app.use((req, res) => {
+    res.end('hello world')
+})
+
+app.listen(3000)
