8c2000aaa30006a1153db2d0966bc2e3ec3299ac
wu-hu-1201 <1871516072@qq.com>
2020-10-25 22:21:14 +0800

    vue-rfc

---
+++ vue/vue-rfc/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div
+      @touchmove="handleMove1"
+      :style="{ position: 'absolute', left: x1 + 'px', top: y1 + 'px' }"
+    >
+      1
+    </div>
+    <div
+      @touchmove="handleMove2"
+      :style="{ position: 'absolute', left: x2 + 'px', top: y2 + 'px' }"
+    >
+      2
+    </div>
+     {{ time }}
+  </div>
+</template>
+
+<script>
+import useDrag from "../useDrag";
+import useClock from '../useClock';
+export default {
+  setup() {
+    const time = useClock();
+    const { x: x1, y: y1, handleMove: handleMove1 } = useDrag();
+    const { x: x2, y: y2, handleMove: handleMove2 } = useDrag();
+    return {
+      x1,
+      x2,
+      y1,
+      y2,
+      handleMove1,
+      handleMove2,
+      time
+    };
+  },
+};
+</script>
+
+<style>
+</style>
