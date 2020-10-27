ea8f00e85756e9edf7b42e7913a82e74d6cd116f
wu-hu-1201 <1871516072@qq.com>
2020-10-27 19:00:14 +0800

    vue-compition

---
+++ vue/vue-compition/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- fragment -->
+    <h2 @touchmove="handleMove" 
+    :style="{left: `${x}px`, top: `${y}px`, position: 'absolute'}">
+    拖拽</h2>
+    <DragAble />
+  </div>
+</template>
+
+<script>
+import withDrag from '../withDrag';
+import Txt from './Txt'
+const DragAble = withDrag(Txt)
+export default {
+  components: {
+    'DragAble': DragAble
+  },
+  data() {
+    return {
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    handleMove(e) {
+      const x = e.touches[0].pageX
+      const y = e.touches[0].pageY
+      this.x = x
+      this.y = y
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-compition/src/components/Txt.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      都是大大
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
