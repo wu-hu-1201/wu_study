535600d5dc4962ec01af9325caf88b0a42de76ce
wu-hu-1201 <1871516072@qq.com>
2020-10-29 21:06:12 +0800

    vue3.0-demo

---
+++ vue/vue3.0/vue3-demo/src/components/Com-demo.vue
@@ -0,0 +1,18 @@
+<template>
+<ul>
+    <FragmentDemo :data="[5, 3, 1, 2, 4, 8]" />
+</ul>
+</template>
+
+<script>
+import FragmentDemo from './Fragment.vue'
+export default {
+    components: {
+        FragmentDemo
+    }
+}
+</script>
+
+<style>
+
+</style>
