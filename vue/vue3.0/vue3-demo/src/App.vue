b0925c29f2d4d729a9a4a5db6c866f638c7b7d82
wu-hu-1201 <1871516072@qq.com>
2020-10-29 21:08:01 +0800

    vue3.0-demo

---
+++ vue/vue3.0/vue3-demo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld msg="Hello Vue 3.0 + Vite" />
+<hr />
+<ComDemo></ComDemo>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ComDemo from './components/Com-demo.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+        ComDemo
+    }
+}
+</script>
