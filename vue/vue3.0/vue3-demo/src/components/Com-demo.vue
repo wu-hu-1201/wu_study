67e716e5991c6f9f757839b99516c3e994d5d9db
wu-hu-1201 <1871516072@qq.com>
2020-10-30 18:23:55 +0800

    vue3.0-demo

--- vue/vue3.0/vue3-demo/src/components/Com-demo.vue
+++ vue/vue3.0/vue3-demo/src/components/Com-demo.vue
@@ -1,14 +1,32 @@
 <template>
 <ul>
     <FragmentDemo :data="[5, 3, 1, 2, 4, 8]" />
+    <hr>
+    <teleport-demo />
+    <hr>
+    <Suspense>
+        <template #default>
+            <AsyncComponent :timeout="3000" />
+            异步组件
+        </template>
+        <template #fallback>
+            <Loading />
+        </template>
+    </Suspense>
 </ul>
 </template>
 
 <script>
 import FragmentDemo from './Fragment.vue'
+import TeleportDemo from './Teleport.vue'
+import AsyncComponent from './Suspense.vue'
+import Loading from './Loading.vue'
 export default {
     components: {
-        FragmentDemo
+        FragmentDemo,
+        TeleportDemo,
+        AsyncComponent,
+        Loading
     }
 }
 </script>
---
+++ vue/vue3.0/vue3-demo/src/components/Teleport.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="confirm-modal">
+    <button @click="isOpen = true">打开</button>
+
+    <Teleport to="#foot-container">
+        <div v-if="isOpen">
+            <p>Vue 鼓励我们通过将 UI 和相关行为封装到组件中来构建 UI</p>
+            <button @click="isOpen = false">取消</button>
+        </div>
+    </Teleport>
+</div>
+</template>
+
+<script>
+import {
+    ref
+} from 'vue'
+export default {
+    setup() {
+        const isOpen = ref(false)
+        return {
+            isOpen
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
