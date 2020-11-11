82ea7e63f9e7bccf7e34fe4d07575a3a7e61094e
wu-hu-1201 <1871516072@qq.com>
2020-11-11 19:02:37 +0800

    webpack打包分析

---
+++ js/webpack-打包分析/main.js
@@ -0,0 +1,37 @@
+(function(modules) {
+    // modules['./index.js']()
+    let installModules = {}
+    function webpack_require(id) {
+        if (installModules[id]) {
+            return installModules[id]
+        }
+        let module = {
+            exports: {}
+        }
+        let exports = module.exports
+        installModules[id] = exports
+        // module.exports === exports 指向了一个对象
+        modules[id](module, exports, webpack_require)
+        // module.exports.add = xxx  别人把add方法加到{}
+        return exports
+    }
+    webpack_require('./index.js')
+})(
+    {
+        './index.js': function(module, exports, require) { // key为文件地址  value为文件内容
+            // index.js里的内容
+            // 引入math.js内容 
+            // 能引入什么东西 就由 require返回值决定
+            const { add } = require('./math.js')
+            // require('./math.js')会去调用math.js
+            console.log(add(4, 5))
+        },
+        './math.js': function(module, exports, require) {
+            // module, exports 对象
+            // math.js里的内容
+            const aa = require('./index.js')
+            module.exports.add = (a, b) => a + b
+            // 对象.add有个add方法
+        }
+    }
+)
