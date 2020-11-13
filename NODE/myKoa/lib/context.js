3fb72994b3f27979a1816b1441c7b867abfb6ce5
wu-hu-1201 <1871516072@qq.com>
2020-11-13 18:35:46 +0800

    mykoa

--- NODE/myKoa/lib/context.js
+++ NODE/myKoa/lib/context.js
@@ -1,4 +1,19 @@
 let proto = {}
 
+function defineGetter(prop, name) {  // prop代表对象， name代表对象上的属性
+    proto.__defineGetter__(name, function() {  // 每个对象都有__defineGetter__这个方法
+        return this[prop][name]  // url == this.request.url
+    }) 
+}
+function defineSetter(prop, name) {
+    proto.__defineSetter__(name, function(val) {  // 每个对象都有__defineGetter__这个方法
+        this[prop][name] = val // url == this.request.url
+    })
+}
+
+defineGetter('request', 'url')
+defineGetter('request', 'path')
+defineGetter('response', 'body')
+defineSetter('response', 'body')
 
 module.exports = proto
--- NODE/myKoa/lib/request.js
+++ NODE/myKoa/lib/request.js
@@ -1,4 +1,16 @@
-let request = {}
+let url = require('url')
+let request = {
+    get url() {  // ctx.request.url  就可以取值了
+        return this.req.url
+    },
+    get path() {
+        return url.parse(this.req.url).pathname
+    },
+    get query() {  // ctx.request.path
+        return url.parse(this.req.url).query
+    }
+    
+}
 
 
 module.exports = request
