51854e9fbab2cc1626a41e6d88f568eee10c5716
wu-hu-1201 <1871516072@qq.com>
2020-11-26 21:36:45 +0800

    tree-shaking

--- js/webpack原理/tree-shaking/src/index.js
+++ js/webpack原理/tree-shaking/src/index.js
@@ -3,6 +3,10 @@ import min from './min.js'
 // import {debounce} from [lodash]  此种情况下tree-shaking会失效，将整个lodash都打包
 // import debounce from 'lodash/debounce'   // 这样写 缩小打包内容，只打包所要用到的debounce
 
+// 入口文件有很多依赖的模块,实际情况中，虽然依赖了某个模块，但其实只使用其中的某些功能。
+// 通过 tree-shaking，将没有使用的模块摇掉，这样来达到删除无用代码的目的。(消除那些引用了但并没有被使用的模块)
+// Tree-shaking 和传统的 DCE的方法又不太一样，传统的DCE 消灭不可能执行的代码，
+// 而Tree-shaking 更关注消除没有用到的代码。
 // 1. es-Module 在代码静态分析的时候，就能知道你引入了哪些模块
 // 2. webpack production 会开启tree-shaking
 // 3. 每个模块里面加入存在(副作用)，剔除它可在package.json中加  "sideEffects": false，
