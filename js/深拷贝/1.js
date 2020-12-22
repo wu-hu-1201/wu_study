5b8c36812325848842661df442719ad6ef47f03a
wu-hu-1201 <1871516072@qq.com>
2020-12-22 22:42:05 +0800

    深拷贝

---
+++ js/深拷贝/1.js
@@ -0,0 +1,12 @@
+function deepClone(o) {
+    let obj = {}
+    for (let k in o) {
+        if (typeof o[k] !== 'object') {
+            obj[k] = o[k]
+        } else {
+            obj[k] = deepClone(o[k])
+        }
+    }
+
+    return obj
+}
