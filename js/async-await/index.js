d4b69a9212deddd5a20d902daf5d147f99c84adf
wu-hu-1201 <1871516072@qq.com>
2021-01-25 22:06:50 +0800

    async-await

--- js/async-await/index.js
+++ js/async-await/index.js
@@ -4,7 +4,7 @@ function getData() {
     }, 500)
 }
 
-// 需要使用上面的异步函数的函数前面加上async， 声明这是一个异步函数
+// 需要使用上面的异步函数的函数前面加上async声明这是一个异步函数
 async function testAsync() {
     // 在异步函数前面加上await，函数执行就会等待await声明的异步函数执行完毕，再往下执行
     try {
